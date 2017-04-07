e39bd6d1831ccd385a0a6bc5213efb6e28afd58c
Magdalena Popińska <magdalena.popinska@bitcomp.fi>
2017-04-07 16:11:35 +0200

    part 2 - done

--- flickr-fetcher-spec.js
+++ flickr-fetcher-spec.js
@@ -68,3 +68,106 @@ describe('#photoObjToURL()', () => {
     expect(actual).to.eql(expected);
   });
 });
+
+// 4
+
+describe('#fetchFlickrData()', () => {
+  it(
+        'should take an API key and fetcher function argument and return a promise for JSON data.',
+        (done) => {
+          const apiKey = 'does not matter much what this is right now';
+          const fakeData = {
+            photos: {
+              page: 1,
+              pages: 2872,
+              perpage: 100,
+              total: '287170',
+              photo: [{
+                id: '24770505034',
+                owner: '97248275@N03',
+                secret: '31a9986429',
+                server: '1577',
+                farm: 2,
+                title: '20160229090898',
+                ispublic: 1,
+                isfriend: 0,
+                isfamily: 0,
+              }, {
+                id: '24770504484',
+                owner: '97248275@N03',
+                secret: '69dd90d5dd',
+                server: '1451',
+                farm: 2,
+                title: '20160229090903',
+                ispublic: 1,
+                isfriend: 0,
+                isfamily: 0,
+              }],
+            },
+          };
+          const fakeFetcher = (url) => {
+            const expectedURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${
+                                 apiKey}&text=pugs&format=json&nojsoncallback=1`;
+            expect(url).to.equal(expectedURL);
+            return Promise.resolve(fakeData);
+          };
+          FlickrFetcher.fetchFlickrData(apiKey, fakeFetcher).then((actual) => {
+            expect(actual).to.eql(fakeData);
+            done();
+          } // ERROR. bez przecinka!
+        );
+        });
+});
+
+// 5
+describe('#fetchPhotos()', () => {
+  it('should take an API key and fetcher function, and return a promise for transformed photos', () => {
+    const apiKey = 'does not matter what this is right now';
+    const expected = [{
+      title: 'Dog goes to desperate measure to avoid walking on a leash',
+      url: 'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg',
+    }, {
+      title: 'the other cate',
+      url: 'https://farm2.staticflickr.com/1514/24765033584_3c190c104e_b.jpg',
+    }];
+    const fakeData = {
+      photos: {
+        page: 1,
+        pages: 2872,
+        perpage: 100,
+        total: '287170',
+        photo: [{
+          id: '25373736106',
+          owner: '99117316@N03',
+          secret: '146731fcb7',
+          server: '1669',
+          farm: 2,
+          title: 'Dog goes to desperate measure to avoid walking on a leash',
+          ispublic: 1,
+          isfriend: 0,
+          isfamily: 0,
+        }, {
+          id: '24765033584',
+          owner: '27294864@N02',
+          secret: '3c190c104e',
+          server: '1514',
+          farm: 2,
+          title: 'the other cate',
+          ispublic: 1,
+          isfriend: 0,
+          isfamily: 0,
+        }],
+      },
+    };
+    const fakeFetcher = (url) => {
+      const expectedURL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${
+                             apiKey}&text=pugs&format=json&nojsoncallback=1`;
+      expect(url).to.equal(expectedURL);
+      return Promise.resolve(fakeData);
+    };
+
+    return FlickrFetcher.fetchPhotos(apiKey, fakeFetcher).then((actual) => {
+      expect(actual).to.eql(expected);
+    });
+  });
+});
--- flickr-fetcher.js
+++ flickr-fetcher.js
@@ -6,11 +6,26 @@ const FlickrFetcher = {
     return `https://farm${photoObj.farm}.staticflickr.com/${photoObj.server}/${photoObj.id}_${
             photoObj.secret}_b.jpg`;
   },
-  transformPhotoObj() {
+  // transformPhotoObj() {
+  //   return {
+  //     title: 'Dog goes to desperate measure to avoid walking on a leash',
+  //     url: 'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg',
+  //   };
+  // },
+  transformPhotoObj(photoObj) {
     return {
-      title: 'Dog goes to desperate measure to avoid walking on a leash',
-      url: 'https://farm2.staticflickr.com/1669/25373736106_146731fcb7_b.jpg',
+      title: photoObj.title,
+      url: FlickrFetcher.photoObjToURL(photoObj),
     };
+  },
+  fetchFlickrData(apiKey, fetch) {
+    const url = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${
+             apiKey}&text=pugs&format=json&nojsoncallback=1`;
+    return fetch(url);
+  },
+  fetchPhotos(apiKey, fetch) {
+    return FlickrFetcher.fetchFlickrData(apiKey, fetch)
+    .then(data => data.photos.photo.map(FlickrFetcher.transformPhotoObj));
   },
 };
 
